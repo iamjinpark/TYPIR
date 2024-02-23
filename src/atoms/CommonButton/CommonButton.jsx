d28600a5fe15796bf3498039611b4cc93660bc72
Lulurem <smile71109@naver.com>
2024-02-23 21:17:08 +0900

    ✨ [feat] CommonButton 컴포넌트 UI 구현 #2

---
+++ src/atoms/CommonButton/CommonButton.jsx
@@ -0,0 +1,4 @@
+function CommonButton() {
+  return <button className="w-[70px] h-[30px] rounded-md bg-black text-white text-[14px]">저장</button>;
+}
+export default CommonButton;
---
+++ src/atoms/CommonButton/CommonButton.stories.jsx
@@ -0,0 +1,9 @@
+import CommonButton from './CommonButton';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: CommonButton,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
