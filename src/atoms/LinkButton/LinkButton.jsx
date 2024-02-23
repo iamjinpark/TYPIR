3063fa082249c9d1a416a6c585975c334b0088f6
Lulurem <smile71109@naver.com>
2024-02-24 04:22:37 +0900

    🎨 [modify] 폰트 사이즈 Props 수정 #7

--- src/atoms/LinkButton/LinkButton.jsx
+++ src/atoms/LinkButton/LinkButton.jsx
@@ -1,4 +1,9 @@
+const size = {
+  '26px': 'text-[26px]',
+  '14px': 'text-[14px]',
+};
+
 function LinkButton({ text, fontSize = '26px' }) {
-  return <button className={`font-serif text-white text-[${fontSize}]`}>{text}</button>;
+  return <button className={`font-serif text-white ${size[fontSize]}`}>{text}</button>;
 }
 export default LinkButton;
