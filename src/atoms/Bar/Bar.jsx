00bbe997d4651c7939d0c4961876228983fb6e83
Lulurem <smile71109@naver.com>
2024-02-24 01:33:05 +0900

    ✨ [feat] Bar 컴포넌트 UI 구현 #6

---
+++ src/atoms/Bar/Bar.jsx
@@ -0,0 +1,4 @@
+function Bar() {
+  return <div className="w-px h-2 bg-white mx-2"></div>;
+}
+export default Bar;
---
+++ src/atoms/Bar/Bar.stories.jsx
@@ -0,0 +1,9 @@
+import Bar from './Bar';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: Bar,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
