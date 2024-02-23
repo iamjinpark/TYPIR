978d39d9dd50293df3872d3a3430272372c6e67a
Lulurem <smile71109@naver.com>
2024-02-24 03:39:42 +0900

    🎨 [modify] 색상 Props 추가 #2

--- src/atoms/CommonButton/CommonButton.jsx
+++ src/atoms/CommonButton/CommonButton.jsx
@@ -1,4 +1,16 @@
-function CommonButton() {
-  return <button className="w-[70px] h-[30px] rounded-md bg-black text-white text-[14px]">저장</button>;
+const bg = {
+  black: 'bg-black',
+  misty: 'bg-misty',
+};
+
+const font = {
+  black: 'text-black',
+  white: 'text-white',
+};
+
+function CommonButton({ text = '저장', bgColor = 'black', fontColor = 'white' }) {
+  return (
+    <button className={`w-[70px] h-[30px] rounded-md ${bg[bgColor]} ${font[fontColor]} text-[12px]`}>{text}</button>
+  );
 }
 export default CommonButton;
