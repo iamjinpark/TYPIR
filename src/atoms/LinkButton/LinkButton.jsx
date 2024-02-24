b61ab8382d0f108d674aaa5f6fb41df55ce0a27e
Lulurem <smile71109@naver.com>
2024-02-25 03:33:32 +0900

    🎨 [modify] 색상 Props 추가 #7

--- src/atoms/LinkButton/LinkButton.jsx
+++ src/atoms/LinkButton/LinkButton.jsx
@@ -1,9 +1,15 @@
+const color = {
+  black: 'text-black',
+  white: 'text-white',
+  misty: 'text-misty',
+};
+
 const size = {
   '26px': 'text-[26px]',
   '14px': 'text-[14px]',
 };
 
-function LinkButton({ text = 'CATEGORY', fontSize = '26px' }) {
-  return <button className={`font-serif text-white ${size[fontSize]}`}>{text}</button>;
+function LinkButton({ text = 'CATEGORY', fontColor = 'white', fontSize = '26px' }) {
+  return <button className={`font-serif ${color[fontColor]} ${size[fontSize]}`}>{text}</button>;
 }
 export default LinkButton;
