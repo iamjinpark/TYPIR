a1f166db17064c494bdf4c763f9c57711f75b7b2
Lulurem <smile71109@naver.com>
2024-02-25 03:42:20 +0900

    🎨 [modify] Fragment 태그 이름 변경 #11

--- src/atoms/ProfileUserName/ProfileUserName.jsx
+++ src/atoms/ProfileUserName/ProfileUserName.jsx
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function ProfileUserName({ text = 'username' }) {
   return (
-    <>
+    <div>
       <div className="font-serif font-bold text-[23px] inline mr-2">{text}</div>
       <FontAwesomeIcon icon={faLock} size="xs" />
-    </>
+    </div>
   );
 }
 export default ProfileUserName;
--- src/atoms/ProfileUserName/ProfileUserName.stories.jsx
+++ src/atoms/ProfileUserName/ProfileUserName.stories.jsx
@@ -3,6 +3,11 @@ import ProfileUserName from './ProfileUserName';
 /**@type{import('@storybook/react').Meta} */
 export default {
   component: ProfileUserName,
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+  },
 };
 
 /**@type{import('@storybook/react').StoryObj} */
