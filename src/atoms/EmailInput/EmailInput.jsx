741af5ec59f5c25e9ed61e1ab5b4c27c074e44cb
yeseul0000 <fiddich999@gmail.com>
2024-02-24 19:21:16 +0900

    ✨ [feat] EmailInput 컴포넌트 구현 FRONTENDSCHOOL8#4

---
+++ src/atoms/EmailInput/EmailInput.jsx
@@ -0,0 +1,44 @@
+import { useState, useRef } from 'react';
+
+// 커스텀 훅을 사용하여 이메일 유효성 검사 로직 분리
+const useEmailValidation = () => {
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  const timeoutId = useRef();
+
+  const validateEmail = (inputEmail) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsValid(emailRegex.test(inputEmail));
+  };
+
+  const handleInputChange = (inputEmail) => {
+    setEmail(inputEmail);
+    clearTimeout(timeoutId.current);
+    timeoutId.current = setTimeout(() => validateEmail(inputEmail), 300);
+  };
+
+  return { email, isValid, handleInputChange };
+};
+
+const EmailInput = () => {
+  const { email, isValid, handleInputChange } = useEmailValidation();
+
+  return (
+    <div>
+      <label className="font-bold	px-2" htmlFor="emailInput">
+        email
+      </label>
+      <input
+        type="text"
+        id="emailInput"
+        value={email}
+        placeholder="email@email.com"
+        onChange={(e) => handleInputChange(e.target.value)}
+        className={`w-72 h-11 mt-1 border border-black block rounded-2xl px-4`}
+      />
+      {!isValid && <p className="text-red-500 px-2  ">유효한 이메일 형식이 아닙니다.</p>}
+    </div>
+  );
+};
+
+export default EmailInput;
