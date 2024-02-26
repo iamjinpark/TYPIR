0238d08934bc5c9dbf6999822b085dbd5662fd7b
Lulurem <smile71109@naver.com>
2024-02-26 17:05:27 +0900

    🎨 [modify] App 컴포넌트 라우팅 설정 #29

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyPage from '@/pages/MyPage/MyPage';
+import AccountManagement from '@/pages/AccountManagement/AccountManagement';
+import Header from '@/molecules/Header/Header';
+import EditProfile from '@/pages/EditProfile/EditProfile';
+
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/account" element={<AccountManagement />} />
+        <Route path="/mypage/editProfile" element={<EditProfile />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
