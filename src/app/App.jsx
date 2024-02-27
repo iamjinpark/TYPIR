4d8bdb858eda031a6a0c3c2631ff81ec85ee465a
iamjinpark <wldls7754@gmail.com>
2024-02-27 16:02:43 +0900

    ✨ [feat] 랜딩-디테일페이지 카테고리 컴포넌트 구현

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,13 @@
 import Category from '@/molecules/Category/Category.jsx';
+import DetailCategoryButton from '@/atoms/DetailCategory/DetailCategory';
+import DetailCategory from '@/molecules/DetailCategory/DetailCategory';
 
 function App() {
   return (
     <div>
-      <Category />
+      {/* <Category /> */}
+      {/* <DetailCategoryButton /> */}
+      <DetailCategory />
     </div>
   );
 }
