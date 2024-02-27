006534cf4f36487e05c54cc3b8bf640ec56cdc4f
yeseul0000 <fiddich999@gmail.com>
2024-02-27 16:39:18 +0900

    ✨ [feat] SocialLogin 컴포넌트 UI 구현 FRONTENDSCHOOL8#35

---
+++ src/atoms/SocialLogin/SocialLogin.stories.jsx
@@ -0,0 +1,9 @@
+import SocialLogin from './SocialLogin';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: SocialLogin,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
