178aa31b392d3ee87a97cd09e97640fb15da2ee6
JIIN PARK <146078235+iamjinpark@users.noreply.github.com>
2024-02-27 18:08:37 +0900

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,1 @@
 
-import Category from '@/molecules/Category/Category.jsx';
-import DetailCategoryButton from '@/atoms/DetailCategory/DetailCategory';
-import DetailCategory from '@/molecules/DetailCategory/DetailCategory';
-
-function App() {
-  return (
-    <div>
-      {/* <Category /> */}
-      {/* <DetailCategoryButton /> */}
-      <DetailCategory />
-    </div>
-  );
-}
-export default App;
