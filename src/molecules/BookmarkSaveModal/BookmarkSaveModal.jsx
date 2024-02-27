ef860e10fe9ef59687698dea0b68d59059642bed
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-02-27 17:38:25 +0900

    🎨 [modify] 북마크 모달 연결 기능 추가

--- src/molecules/BookmarkSaveModal/BookmarkSaveModal.jsx
+++ src/molecules/BookmarkSaveModal/BookmarkSaveModal.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import AddBookmark from "@/atoms/AddBookmark/AddBookmark";
 import BookmarkList from "@/atoms/BookmarkList/BookmarkList";
 import CloseButton from "@/atoms/CloseButton/CloseButton";
+import BookmarkInputModal from '../BookmarkInputModal/BookmarkInputModal';
 
 const BookmarkSaveModal = () => {
+  const [openInputModal, setOpenInputModal] = useState(false)
+  const [list, setList] = useState(["프로필에 저장", "북마크 1", "북마크 2"])
+
+  const handleAddBookmark = (bookmarkName) => {
+    setList([...list, bookmarkName])
+  };
+
+  const openModal = () => {
+    setOpenInputModal(true)
+  };
+
+  const closeModal = () => {
+    setOpenInputModal(false)
+  };
+
   return (
-    <div className="relative w-[320px] h-[350px] border border-black rounded-xl flex flex-col  overflow-auto">
+    <div className="relative w-[320px] h-[350px] border border-black rounded-xl flex flex-col overflow-auto">
       <p className=" absolute font-semibold top-3 w-full text-center">저장</p> 
       <CloseButton />
       <div className="mt-14 ml-3">
@@ -14,13 +31,13 @@ const BookmarkSaveModal = () => {
         </ul>
         <ul className="text-sm mb-5">
           <p className="mb-2">기존 북마크에 추가</p>
-            <BookmarkList title="북마크 1"/>
-            <BookmarkList title="북마크 2"/>
-            <BookmarkList title="북마크 3"/>
-            <BookmarkList title="북마크 4"/>
+          {list.map((bookmark, index) => (
+            <BookmarkList key={index} title={bookmark} />
+          ))}
         </ul>
       </div>   
-      <AddBookmark />
+      {openInputModal && <BookmarkInputModal onAddBookmark={handleAddBookmark} onClose={closeModal} />}
+      <AddBookmark onClick={openModal} />
     </div>
   );
 };
