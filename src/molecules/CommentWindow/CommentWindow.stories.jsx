6e719a32a6312d7f7272ef99ceb3cff0c5fd3e4e
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-02-27 09:14:43 +0900

    ✨[feat] CommentWindow UI 구현

---
+++ src/molecules/CommentWindow/CommentWindow.stories.jsx
@@ -0,0 +1,9 @@
+import CommentWindow from './CommentWindow';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: CommentWindow,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
