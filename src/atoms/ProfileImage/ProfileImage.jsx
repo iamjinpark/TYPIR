a31524ad39eabae04ceecb25438d06f714545bf0
Lulurem <smile71109@naver.com>
2024-02-27 16:51:00 +0900

    ✨ [feat] 이미지 파일 선택 기능 추가 #3

--- src/atoms/ProfileImage/ProfileImage.jsx
+++ src/atoms/ProfileImage/ProfileImage.jsx
@@ -1,4 +1,30 @@
+import { useRef } from 'react';
+import create from 'zustand';
+
+const useStore = create((set) => ({
+  selectedFile: null,
+  setSelectedFile: (file) => set({ selectedFile: file }),
+}));
+
 function ProfileImage({ width = 'w-[120px]', height = 'h-[120px]', border = 'rounded-full', bgColor = 'bg-gray-100' }) {
-  return <div className={`${width} ${height} ${border} ${bgColor}`}></div>;
+  const fileInputRef = useRef();
+  const selectedFile = useStore((state) => state.selectedFile);
+  const setSelectedFile = useStore((state) => state.setSelectedFile);
+
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(URL.createObjectURL(file));
+  };
+
+  return (
+    <div className={`${width} ${height} ${border} ${selectedFile ? '' : bgColor}`} onClick={handleImageClick}>
+      {selectedFile && <img src={selectedFile} alt="프로필 이미지" />}
+      <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />
+    </div>
+  );
 }
 export default ProfileImage;
