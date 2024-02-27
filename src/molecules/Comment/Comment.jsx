13f56693f5952fe3d1cfea1e91a190149bcb0e6f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-02-27 17:36:04 +0900

    🎨 [modify] Comment props 변경

--- src/molecules/Comment/Comment.jsx
+++ src/molecules/Comment/Comment.jsx
@@ -1,12 +1,12 @@
-const Comment = () => {
+const Comment = ({ userName, text, daysAgo }) => {
   return (
     <div className="comment flex items-center">
-      <img src="/public/profile/profile.svg" className="w-[40px] h-[40px]"/>
+      <img src="/images/profile.svg" className="w-[40px] h-[40px]"/>
       <div className="ml-3 ">
-        <span className="username font-bold">dannxnni</span>
-        <span className="comment_text ml-3">정보 좀요</span>
+        <span className="username font-bold">{userName}</span>
+        <span className="comment_text ml-3">{text}</span>
         <div className="flex gap-3 text-[#909090]">
-          <p className="days_ago">3일전</p>
+          <p className="days_ago">{daysAgo}</p>
           <a>답글쓰기</a>
         </div>
       </div>
