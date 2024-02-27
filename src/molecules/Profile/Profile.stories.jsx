208c5364b49cbcd9014973f3336330d5b4a7d87e
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-02-27 09:22:49 +0900

    ✨ [feat] : Profile UI 구현

---
+++ src/molecules/Profile/Profile.stories.jsx
@@ -0,0 +1,9 @@
+import Profile from './Profile';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: Profile,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
