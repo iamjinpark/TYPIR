2ceeebe7a20957b03755f7091d0aa26cd8b315ad
yeseul0000 <fiddich999@gmail.com>
2024-02-27 16:51:49 +0900

    ✨ [feat] ModifyProfileImg 컴포넌트 UI 구현 FRONTENDSCHOOL8#38

---
+++ src/atoms/ModifyProfileImg/ModifyProfileImg.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useRef } from 'react';
+
+const ModifyProfileImg = () => {
+  const [image, setImage] = useState(null);
+  const [preview, setPreview] = useState('');
+  const fileInputRef = useRef(null);
+
+  const handleAddImage = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const newImage = URL.createObjectURL(file);
+      setImage(file);
+      setPreview(newImage);
+    }
+  };
+
+  return (
+    <div
+      className="w-32 h-32 bg-gray-300 flex flex-col items-center justify-center relative rounded-full cursor-pointer overflow-hidden"
+      onClick={() => fileInputRef.current.click()}
+    >
+      <input type="file" ref={fileInputRef} onChange={handleAddImage} className="hidden" accept="image/*" />
+      {preview ? (
+        <img src={preview} alt="Selected image" className="w-full h-full object-cover rounded-full" />
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-8 w-8 text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v12m6-6H6" />
+          </svg>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ModifyProfileImg;
---
+++ src/atoms/ModifyProfileImg/ModifyProfileImg.stories.jsx
@@ -0,0 +1,9 @@
+import ModifyProfileImg from './ModifyProfileImg';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: ModifyProfileImg,
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
