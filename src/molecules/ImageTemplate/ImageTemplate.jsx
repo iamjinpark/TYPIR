654266763ef867b9384009e9700e00bdc865c539
Lulurem <smile71109@naver.com>
2024-02-28 23:26:01 +0900

    ✨ [feat] ImageTemplate 반응형 레이아웃 구현 #25

--- src/molecules/ImageTemplate/ImageTemplate.jsx
+++ src/molecules/ImageTemplate/ImageTemplate.jsx
@@ -3,20 +3,22 @@ import images from '/src/data/images.json';
 
 function ImageTemplate() {
   const breakpointColumnsObj = {
-    default: 2,
+    default: 4,
+    650: 3,
+    500: 2,
   };
 
   return (
-    <div className="w-full h-auto bg-white">
+    <div className="max-w-screen-md h-auto bg-white">
       <Masonry
         breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid flex gap-[10px]"
+        className="my-masonry-grid flex gap-[12px]"
         columnClassName="my-masonry-grid_column flex flex-col items-center"
       >
         {images.map((item) => (
           <div
             key={item.id}
-            className={`w-full bg-gray-100 rounded-2xl mb-[15px]`}
+            className={`w-[170px] bg-gray-100 rounded-2xl mb-[15px]`}
             style={{ height: `${item.height}px` }}
           >
             {item.content}
