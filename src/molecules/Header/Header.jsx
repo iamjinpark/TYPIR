4df4e3df8f1f28ca5bfd91be648b7627ae3e311f
Lulurem <smile71109@naver.com>
2024-02-28 23:38:57 +0900

    ✨ [feat] sticky 헤더 기능 구현 #18

--- src/molecules/Header/Header.jsx
+++ src/molecules/Header/Header.jsx
@@ -1,14 +1,14 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SelectPageModal from '../SelectPageModal/SelectPageModal';
-import useStore from '@/hooks/useStore';
+import { useSelectCategoryStore } from '@/hooks/useStore';
 import Logo from '@/atoms/Logo/Logo';
 
 function Header() {
-  const { isModalOpen, openModal } = useStore();
+  const { isModalOpen, openModal } = useSelectCategoryStore();
 
   return (
-    <div className="w-80 h-12 bg-white flex justify-between items-center p-4" style={{ position: 'relative' }}>
+    <div className="w-full h-[48px] bg-white flex justify-between items-center p-4 sticky top-0" style={{ zIndex: 20 }}>
       <Logo size="w-20" />
       <FontAwesomeIcon icon={faBars} size="lg" onClick={openModal} />
       {isModalOpen && <SelectPageModal />}
