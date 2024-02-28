1c330bc8cc3a1b9af53306519ef3ffef3fed710b
yeseul0000 <fiddich999@gmail.com>
2024-02-28 17:16:17 +0900

    💄 [style] LoginForm 컴포넌트 간격 조정(임시) FRONTENDSCHOOL8#39

--- src/molecules/LoginForm/LoginForm.jsx
+++ src/molecules/LoginForm/LoginForm.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import EmailInput from '@/atoms/EmailInput/EmailInput';
 import PasswordInput from '@/atoms/PasswordInput/PasswordInput';
 import Checkbox from '@/atoms/Checkbox/Checkbox';
@@ -5,14 +6,25 @@ import SubmitButton from '@/atoms/SubmitButton/SubmitButton';
 
 function LoginForm() {
   return (
-    <>
-      <form action="">
+    <form action="" className="space-y-4">
+      <div className="mb-4">
+        {' '}
+        {/* EmailInput 아래 16px */}
         <EmailInput />
+      </div>
+      <div className="mb-14">
+        {' '}
+        {/* PasswordInput 아래 16px */}
         <PasswordInput />
+      </div>
+      <div className="mb-15">
+        {' '}
+        {/* Checkbox에서 44px 아래 */}
         <Checkbox />
-        <SubmitButton />
-      </form>
-    </>
+      </div>
+      {/* SubmitButton */}
+      <SubmitButton />
+    </form>
   );
 }
 
