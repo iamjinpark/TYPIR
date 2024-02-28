95d00474088e5e84a5dc41b0ac267b04e29afc06
Lulurem <smile71109@naver.com>
2024-02-28 16:16:01 +0900

    ✨ [feat] 이전 페이지로 이동 기능 추가 #50

--- src/atoms/Backward/Backward.jsx
+++ src/atoms/Backward/Backward.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from 'react-router-dom';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Backward() {
-  return <FontAwesomeIcon icon={faArrowLeft} size="lg" />;
+  const navigate = useNavigate();
+
+  return <FontAwesomeIcon icon={faArrowLeft} size="lg" onClick={() => navigate(-1)} style={{ cursor: 'pointer' }} />;
 }
 export default Backward;
