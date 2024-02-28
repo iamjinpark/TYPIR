f5368ec27aacd8401aafc6bd1d106516091937b3
Lulurem <smile71109@naver.com>
2024-02-28 17:07:21 +0900

    🎨 [modify] 모달 적용 영역 변경 #8

--- src/molecules/SelectPageModal/SelectPageModal.jsx
+++ src/molecules/SelectPageModal/SelectPageModal.jsx
@@ -20,16 +20,16 @@ function SelectPageModal() {
   };
 
   return (
-    <div className="w-80 h-[650px] bg-gray-700 z-10" style={{ position: 'absolute', top: 0, left: 0 }}>
+    <div className="w-full h-screen bg-gray-700 z-10" style={{ position: 'absolute', top: 0, left: 0 }}>
       <div className="flex justify-end items-start pt-4 pr-4">
         <FontAwesomeIcon icon={faXmark} color="white" onClick={closeModal} />
       </div>
-      <div className="h-[570px] flex flex-col gap-6 justify-center">
+      <div className="h-5/6 flex flex-col gap-6 justify-center items-center">
         <LinkButton text="CATEGORY" />
         <LinkButton text="COMMUNITY" />
         <LinkButton text="MY PAGE" onClick={handleMyPageClick} />
       </div>
-      <div className="flex justify-center items-center">
+      <div className="h-1/6 flex justify-center items-center pb-14">
         <LinkButton text="New Post" fontSize="text-[14px]" hoverScale="" onClick={handleNewPostClick} />
         <Bar />
         <LinkButton text="Logout" fontSize="text-[14px]" hoverScale="" />
