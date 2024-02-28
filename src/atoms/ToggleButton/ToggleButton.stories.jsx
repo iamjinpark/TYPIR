11f7ad43763520ea60c515e17de521712e0fc52d
Lulurem <smile71109@naver.com>
2024-02-28 23:24:10 +0900

    ✨ [feat] Togglebutton 컴포넌트 UI 구현 #59

---
+++ src/atoms/ToggleButton/ToggleButton.stories.jsx
@@ -0,0 +1,14 @@
+import ToggleButton from './ToggleButton';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: ToggleButton,
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+  },
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
