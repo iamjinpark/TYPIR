03950893b6f3f250b7436ef1a1004ac1178a8fc9
Lulurem <smile71109@naver.com>
2024-02-28 15:48:49 +0900

    ✨ [feat] Backward 컴포넌트 UI 구현 #50

---
+++ src/atoms/Backward/Backward.jsx
@@ -0,0 +1,7 @@
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function Backward() {
+  return <FontAwesomeIcon icon={faArrowLeft} size="lg" />;
+}
+export default Backward;
---
+++ src/atoms/Backward/Backward.stories.jsx
@@ -0,0 +1,14 @@
+import Backward from './Backward';
+
+/**@type{import('@storybook/react').Meta} */
+export default {
+  component: Backward,
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+  },
+};
+
+/**@type{import('@storybook/react').StoryObj} */
+export const 기본표시 = {};
