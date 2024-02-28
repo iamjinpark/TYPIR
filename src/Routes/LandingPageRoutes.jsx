e8d198d836c11a91d9c32030cc9abcd9dff62581
yeseul0000 <fiddich999@gmail.com>
2024-02-28 17:23:43 +0900

    🎨 [modify] LandingPageRoutes 라우트 코드 추가

--- src/Routes/LandingPageRoutes.jsx
+++ src/Routes/LandingPageRoutes.jsx
@@ -1,9 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import Landing from '@/pages/Landing/Landing';
+import SignIn from '@/pages/SignIn/SignIn';
+import SignUp from '@/pages/SignUp/SignUp';
+import SetInitialProfile from '@/pages/SetInitialProfile/SetInitialProfile';
 
 function LandingPageRoutes() {
   return (
     <Routes>
-      <Route></Route>
+      <Route path="/landing" element={<Landing />} />
+      <Route path="/landing/signin" element={<SignIn />} />
+      <Route path="/landing/signup" element={<SignUp />} />
+      <Route path="/landing/signup/setting" element={<SetInitialProfile />} />
     </Routes>
   );
 }
