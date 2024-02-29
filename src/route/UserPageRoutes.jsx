71a637e3eac455e9fde36b5e9ca62c8ce905331f
yeseul0000 <fiddich999@gmail.com>
2024-02-29 10:13:12 +0900

    🎨 [modify] splash 페이지 라우트 설정

--- src/route/UserPageRoutes.jsx
+++ src/route/UserPageRoutes.jsx
@@ -1,9 +1,16 @@
+import SetInitialProfile from '@/pages/SetInitialProfile/SetInitialProfile';
+import SignIn from '@/pages/SignIn/SignIn';
+import SignUp from '@/pages/SignUp/SignUp';
+import Splash from '@/pages/Splash/Splash';
 import { Route, Routes } from 'react-router-dom';
 
 function UserPageRoutes() {
   return (
     <Routes>
-      <Route></Route>
+      <Route path="/splash/" element={<Splash />} />
+      <Route path="/splash/signin" element={<SignIn />} />
+      <Route path="/splash/signup" element={<SignUp />} />
+      <Route path="/splash/setprofile" elemet={<SetInitialProfile />} />
     </Routes>
   );
 }
