0082740294eb97321205a24e2a99de324007b411
yeseul0000 <fiddich999@gmail.com>
2024-02-29 09:35:04 +0900

    ⚡️  [refactoring] 주석 삭제 등 코드 정리 FRONTENDSCHOOL8#51

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -6,32 +6,15 @@ import SubmitButton from '@/atoms/SubmitButton/SubmitButton';
 
 const SignUp = () => {
   return (
-    // 전체 컨테이너를 320x650px 크기로 설정하고, 화면 중앙에 배치합니다.
     <div className="w-[320px] h-[650px] mx-auto flex flex-col items-center justify-start">
-      {/* AccountPageTitle 컴포넌트 상단 여백 75px 적용 */}
-      <div className="mt-75px mb-43px">
-        <AccountPageTitle text="회원가입" />
-      </div>
+      <AccountPageTitle text="회원가입" className={'mt-75px mb-43px'} />
       <form action="" className="w-full flex flex-col items-center">
-        {/* EmailInput 컴포넌트 */}
         <EmailInput />
-        {/* EmailInput과 PasswordInput 사이 간격 38px 적용 */}
-        <div className="mt-18px">
-          <PasswordInput />
-        </div>
-        {/* PasswordInput "패스워드 확인"과 간격을 동일하게 유지 */}
-        <div className="mt-18px">
-          <PasswordInput text="패스워드 확인" />
-        </div>
-        {/* SubmitButton 컴포넌트 */}
-        <div className="mt-31px">
-          <SubmitButton text="회원가입" />
-        </div>
+        <PasswordInput className={'mt-18px'} />
+        <PasswordInput text="패스워드 확인" className={'mt-18px'} />
+        <SubmitButton text="회원가입" className={'mt-31px'} />
       </form>
-      {/* SocialLogin 컴포넌트는 폼 아래에 위치 */}
-      <div className="mt-32px">
-        <SocialLogin />
-      </div>
+      <SocialLogin className={'mt-32px'} />
     </div>
   );
 };
