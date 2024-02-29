9e48e703022042969429e0b10c95422cefad84ee
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-02-29 13:53:13 +0900

     🎨 [modify] 댓글 기능 구현 중

--- src/pages/CommunityDetail/CommunityDetail.jsx
+++ src/pages/CommunityDetail/CommunityDetail.jsx
@@ -1,4 +1,3 @@
-// CommunityDetail.jsx
 import BookmarkButton from "@/atoms/BookmarkButton/BookmarkButton";
 import CommentCount from "@/atoms/CommentCount/CommentCount";
 import HeartButton from "@/atoms/HeartButton/HeartButton";
@@ -10,16 +9,16 @@ import Header from "@/molecules/Header/Header";
 import { useState } from "react";
 
 const CommunityDetail = ({ count = 3 }) => {
-  const [likeCount, setLikeCount] = useState(count);
+  const [likeCount, setLikeCount] = useState(count)
   const [comment, setComment] = useState([]);
 
-  const handleLikeChange = (val) => {
-    setLikeCount(prevCount => prevCount + val);
+  const handleLikeChange = (change) => {
+    setLikeCount((prevCount) => prevCount + change)
   };
 
   const handleAddComment = (newComment) => {
-    setComment(prevComment => [...prevComment, newComment])
-  }
+    setComment((prevComments) => [...prevComments, newComment])
+  };
 
   return (
     <div className="w-[320px] mx-auto">
@@ -32,16 +31,14 @@ const CommunityDetail = ({ count = 3 }) => {
         </div>
         <div className="flex justify-between mb-[5px]">
           <HeartCount count={likeCount} />
-          <CommentCount count="4" />
+          <CommentCount count={comment.length} />
         </div>
         <TextContents fontSize="14" text="this is content this is content this is content this is content this is content this is content this is content this is content this is content this is content this is content this is content ✨"/>
         <div className="w-full h-px bg-[#C4C4C4] my-[10px]"></div>
-        {/* <Comment userName="dannxnni" text="Good~!" daysAgo="2일 전"/>
-        <Comment userName="dkssudgktpdu" text="할 게 너무너무너무 많다!dkfjdslkfjdslkfjdskjflksjflds" daysAgo="3일 전"/> */}
-        {comment.map((comment, idx) => (
-          <Comment key={idx} userName={comment.userName} text={comment.text} daysAgo={comment.daysAgo} />
+        {comment.map((comment, index) => (
+          <Comment key={index} userName={comment.userName} text={comment.text} daysAgo={comment.daysAgo}/>
         ))}
-        <CommentWindow onAddComment={handleAddComment}/>
+        <CommentWindow onAddComment={handleAddComment} />
       </div>
     </div>
   );
