51aea0fc08089f87beddc3cdab13d52f1704b1cf
yeseul0000 <fiddich999@gmail.com>
2024-02-29 10:13:47 +0900

    🎨 [modify] a를 Link로 변경 FRONTENDSCHOOL8#56

--- src/atoms/SignInSignUpLinks/SignInSignUpLinks.jsx
+++ src/atoms/SignInSignUpLinks/SignInSignUpLinks.jsx
@@ -1,13 +1,15 @@
-function SignInSignUpLinks({ text = '로그인' }) {
+import { Link } from 'react-router-dom';
+
+function SignInSignUpLinks({ text = '로그인', routeLink = '/splash/signin' }) {
   return (
-    <a
-      href="#"
+    <Link
+      to={routeLink}
       className={`w-72 h-11 mt-1 flex justify-center items-center rounded-2xl px-4 font-bold ${
         text === '로그인' ? 'bg-black text-white' : 'bg-white text-black border border-black'
       }`}
     >
       {text}
-    </a>
+    </Link>
   );
 }
 
