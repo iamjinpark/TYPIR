c10a8788827c77540522774add85c4e597048414
Lulurem <smile71109@naver.com>
2024-03-03 00:08:41 +0900

    ✨ [feat] 디테일 연결 애니메이션 추가, 사용처별 경로 설정 #25

--- src/molecules/ImageTemplate/ImageTemplate.jsx
+++ src/molecules/ImageTemplate/ImageTemplate.jsx
@@ -1,10 +1,26 @@
 import Masonry from 'react-masonry-css';
 import images from '/src/data/images.json';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useLocation } from 'react-router-dom';
 
 function ImageTemplate() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onBoxClicked = (imageId) => {
+    if (location.pathname.endsWith('/category')) {
+      navigate(`/category/detail/${imageId}`);
+    } else if (location.pathname.endsWith('/mypage')) {
+      navigate(`/mypage/detail/${imageId}`);
+    } else if (location.pathname.includes('/newpost')) {
+      navigate(`/mypage/newpost/detail/${imageId}`);
+    }
+  };
+
   const breakpointColumnsObj = {
     default: 4,
-    650: 3,
+    639: 3,
     500: 2,
   };
 
@@ -16,13 +32,15 @@ function ImageTemplate() {
         columnClassName="my-masonry-grid_column flex flex-col items-center"
       >
         {images.map((item) => (
-          <div
+          <motion.div
             key={item.id}
-            className={`w-[170px] bg-gray-100 rounded-2xl mb-[15px]`}
+            className={`w-[170px] bg-gray-100 rounded-2xl mb-[15px] cursor-zoom-in`}
             style={{ height: `${item.height}px` }}
+            onClick={() => onBoxClicked(item.id)}
+            layoutId={item.id + ''}
           >
             {item.content}
-          </div>
+          </motion.div>
         ))}
       </Masonry>
     </div>
