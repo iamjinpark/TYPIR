2546152de8ec319ffbf5abcb75fc06b0c37a8ff1
Lulurem <smile71109@naver.com>
2024-03-02 22:16:03 +0900

    🦴 [chore] useDetailModalStore 추가 #5

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 
+/* 마이 페이지, 헤더 관련 */
 export const useSelectCategoryStore = create((set) => ({
   isModalOpen: false,
   openModal: () => set({ isModalOpen: true }),
@@ -7,6 +8,12 @@ export const useSelectCategoryStore = create((set) => ({
 }));
 
 export const useDeleteModalStore = create((set) => ({
+  isModalOpen: false,
+  openModal: () => set({ isModalOpen: true }),
+  closeModal: () => set({ isModalOpen: false }),
+}));
+
+export const useDetailModalStore = create((set) => ({
   isModalOpen: false,
   openModal: () => set({ isModalOpen: true }),
   closeModal: () => set({ isModalOpen: false }),
