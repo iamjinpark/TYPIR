aa029f1df04c1e9b1ccc050a3aa494bc97bcba58
yeseul0000 <fiddich999@gmail.com>
2024-03-03 14:08:29 +0900

    🎨 [modify] 글씨 클릭해도 체크박스 체크 되도록 수정 FRONTENDSCHOOL8#36

--- src/atoms/Checkbox/Checkbox.jsx
+++ src/atoms/Checkbox/Checkbox.jsx
@@ -1,16 +1,26 @@
 import { useState } from 'react';
 
-function Checkbox({ content = '로그인 상태 유지', className }) {
+function Checkbox({ text = '로그인 상태 유지', className }) {
   const [isChecked, setIsChecked] = useState(false);
 
   const handleCheckboxChange = (event) => {
     setIsChecked(event.target.checked);
   };
 
+  const checkboxId = `checkbox-${text.replace(/\s+/g, '-').toLowerCase()}`;
+
   return (
-    <div className={`flex items-center ${className}`}>
-      <input type="checkbox" checked={isChecked} onChange={handleCheckboxChange} className="accent-black h-4 w-4" />
-      <span className="ml-2">{content}</span>
+    <div className={`flex items-center px-2 ${className}`}>
+      <input
+        type="checkbox"
+        id={checkboxId}
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+        className="accent-black h-4 w-4"
+      />
+      <label htmlFor={checkboxId} className="ml-2 cursor-pointer">
+        {text}
+      </label>
     </div>
   );
 }
