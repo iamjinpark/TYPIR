fdb07f4d3a2ff49fcd2862b4ed557f17302aedc6
yeseul0000 <fiddich999@gmail.com>
2024-03-03 13:30:09 +0900

    💄 [style] Splash 디자인 수정 FRONTENDSCHOOL8#53

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -5,18 +5,18 @@ import SignInSignUpLinks from '@/atoms/SignInSignUpLinks/SignInSignUpLinks';
 const Splash = () => {
   return (
     <div
-      className="w-[320px] h-[650px] flex flex-col items-center px-15 py-0 m-auto relative"
+      className="min-w-[360px] max-w-[768px] h-[650px] flex flex-col items-center px-15 py-0 m-auto relative"
       style={{
         backgroundImage: "url('/public/images/splash_bg.png')",
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
     >
-      <div className="absolute top-0 left-0 w-full h-full" style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
 
       <div className="relative z-10 w-full h-full">
-        <div className="absolute top-[90px] left-[34px]">
-          <Logo size="w-203px" color="white" />
+        <div className="absolute top-[90px] left-[34px] md:top-[120px] md:left-[50px]">
+          <Logo size="w-203pxr" color="white" />
           <div className="text-white mt-4 text-2xl">
             <p>Turn</p>
             <p>Your Pinterest</p>
@@ -24,11 +24,9 @@ const Splash = () => {
             <p>Reality</p>
           </div>
         </div>
-        <div className="absolute bottom-20 left-0 right-0 flex flex-col items-center">
+        <div className="absolute bottom-20 left-0 right-0 flex flex-col items-center mx-15px">
           <SignInSignUpLinks text="로그인" />
-          <div className="mt-3">
-            <SignInSignUpLinks text="회원가입" />
-          </div>
+          <SignInSignUpLinks text="회원가입" />
         </div>
       </div>
     </div>
