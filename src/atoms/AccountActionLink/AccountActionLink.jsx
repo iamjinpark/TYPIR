7a4cefec861001910b5a384900874b4a92b3bf16
yeseul0000 <fiddich999@gmail.com>
2024-03-03 14:00:19 +0900

    🎨 [modify] ask prop 내용에따라 text내용 변경되도록 수정 FRONTENDSCHOOL8#28

--- src/atoms/AccountActionLink/AccountActionLink.jsx
+++ src/atoms/AccountActionLink/AccountActionLink.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const AccountActionLink = ({ ask = '계정이 없으신가요?', content = '회원가입', router = '/signup' }) => (
-  <div>
-    {ask} <Link to={router}>{content}</Link>
-  </div>
-);
+const AccountActionLink = ({ ask, text = '회원가입', router = '/signup' }) => {
+  const askMessage = text === '로그인' ? '계정이 있으신가요?' : '계정이 없으신가요?';
+
+  return (
+    <div className="mt-15px">
+      {ask ? ask : askMessage}{' '}
+      <a href={router} className="text-gray-300">
+        {text}
+      </a>
+    </div>
+  );
+};
 
 export default AccountActionLink;
