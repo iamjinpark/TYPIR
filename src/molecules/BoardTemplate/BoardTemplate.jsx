7d4ae8fc4c1072f4065a936d6350975a9055c5d9
Lulurem <smile71109@naver.com>
2024-03-05 01:18:48 +0900

    ✨ [feat] 보드 DB 렌더링 #19

--- src/molecules/BoardTemplate/BoardTemplate.jsx
+++ src/molecules/BoardTemplate/BoardTemplate.jsx
@@ -1,21 +1,24 @@
 import TextContents from '@/atoms/TextContents/TextContents';
-import images from '/src/data/images.json';
 
-function BoardItem({ content, span }) {
+function BoardItem({ imageSrc, span }) {
   const width = span ? 'calc(47px * 2)' : '47px';
   const itemHeight = span ? 'calc(47px * 2)' : '47px';
 
-  return <div className={`w-[${width}] h-[${itemHeight}] bg-gray-100 ${span}`}>{content}</div>;
+  return (
+    <div className={`w-[${width}] h-[${itemHeight}] ${span}`}>
+      <img src={imageSrc} className="w-full h-full object-cover" />
+    </div>
+  );
 }
 
-function BoardTemplate({ text, onBoardClick }) {
+function BoardTemplate({ text, images, onBoardClick }) {
   return (
     <div>
       <TextContents text={text} fontFamily="font-serif" fontWeight="font-bold" fontSize="text-[18px]" padding="p-2" />
       <div className="w-[280px] h-[170px] rounded-2xl overflow-hidden" onClick={() => onBoardClick(text.toLowerCase())}>
         <div className="w-full h-full grid grid-cols-3 grid-rows-2 gap-1px">
           {images.slice(0, 3).map((image, index) => (
-            <BoardItem key={image.id} {...image} span={index === 0 ? 'col-span-2 row-span-2' : ''} />
+            <BoardItem key={image} imageSrc={image} span={index === 0 ? 'col-span-2 row-span-2' : ''} />
           ))}
         </div>
       </div>
