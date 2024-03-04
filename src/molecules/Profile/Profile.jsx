5fd701048a5506314dcb4d271ba2975e8467b855
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-03-04 12:48:00 +0900

    🎨 [modify] 작성자 구분하여 모달 창 open

--- src/molecules/Profile/Profile.jsx
+++ src/molecules/Profile/Profile.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import UserModal from "../UserModal/UserModal";
+import WriterModal from "../WriterModal/WriterModal";
+
 const Profile = ({ userName }) => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsOpen(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <div className="w-[320px] flex items-center">
+    <div className="w-[320px] flex items-center justify-center">
       <img src="/images/profile.svg" className="w-[45px] h-[45px] ml-[15px]"/>
       <p className="ml-3">{userName}</p>
-      <button className="ml-auto mr-[15px]">
+      <button onClick={handleOpenModal} className="ml-auto mr-[15px]">
         <img src="/images/plus.svg" className="w-[18px]"/>
       </button>
+        {/* {isOpen && <UserModal onClose={handleCloseModal} />} */}
+        {isOpen && <WriterModal onClose={handleCloseModal}/>}
     </div>
   );
 };
