34cd202cf533779a347415a9f39e3ad012b72682
yeseul0000 <fiddich999@gmail.com>
2024-03-05 09:12:45 +0900

    ✨ [feat] validation 추가  FRONTENDSCHOOL8#26

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -4,8 +4,15 @@ import CommonInput from '@/atoms/CommonInput/CommonInput';
 import HandleText from '@/atoms/HandleText/HandleText';
 import ProfileImage from '@/atoms/ProfileImage/ProfileImage';
 import TextContents from '@/atoms/TextContents/TextContents';
+import { useState } from 'react';
 
 function EditProfile() {
+  const [nickname, setNickname] = useState('');
+  const [handle, setHandle] = useState('');
+
+  const isNameValid = /^[a-zA-Z]+$/.test(nickname) && nickname.length >= 3 && nickname.length <= 16;
+  const isHandleValid = /^[a-zA-Z]+$/.test(handle) && handle.length >= 3 && handle.length <= 16;
+
   return (
     <div className="w-full h-[570px] bg-white flex flex-col items-center mb-8">
       <div className="w-full flex justify-between p-5">
@@ -16,11 +23,20 @@ function EditProfile() {
         <div className="flex-1"></div>
       </div>
       <ProfileImage />
-      <CommonInput text="닉네임" />
-      <CommonInput text="핸들" />
-      <div className="w-[290px] flex pt-1 pl-2 mb-9">
-        <HandleText text="www.typir.com/girls_day" />
+      <CommonInput
+        text="닉네임"
+        value={nickname}
+        onChange={(e) => setNickname(e.target.value)}
+        placeholder="커뮤니티에서 사용할 닉네임"
+      />
+      <div className="text-red-500 text-xs h-1 -translate-x-7">
+        {!isNameValid && nickname.length > 0 && '3글자 이상, 16글자 이하의 영문이어야 합니다.'}
       </div>
+      <CommonInput text="핸들" value={handle} onChange={(e) => setHandle(e.target.value)} placeholder="개인페이지 ID" />
+      <div className="text-red-500 text-xs h-1 -translate-x-7 mb-9">
+        {!isHandleValid && handle.length > 0 && '3글자 이상, 16글자 이하의 영문이어야 합니다.'}
+      </div>
+
       <CommonButton />
     </div>
   );
