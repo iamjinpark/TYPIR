314897789f7471041c8d9f17cd86cf544a47f28d
iamjinpark <wldls7754@gmail.com>
2024-03-05 11:24:06 +0900

    ✨ [feat] : 상세이미지 id 연동 완료

--- src/pages/Rending/Rending.jsx
+++ src/pages/Rending/Rending.jsx
@@ -2,15 +2,18 @@ import DetailImage from '@/molecules/DetailImage/DetailImage';
 import Category from '@/molecules/Category/Category';
 import ImageTemplate from '@/molecules/ImageTemplate/ImageTemplate';
 import { useMatch } from 'react-router-dom';
+import { useStyleStore } from '@/zustand/useStore';
 
 const Rending = () => {
   const categoryImageMatch = useMatch('/category/detail/:imageId');
   const layoutId = categoryImageMatch?.params.imageId;
+  const styles = useStyleStore((state) => state.styles);
+  const imageSrc = styles.find((style) => style.id === layoutId)?.image;
 
   return (
     <div className="template">
       <Category />
-      {categoryImageMatch ? <DetailImage layoutId={layoutId} /> : null}
+      {categoryImageMatch ? <DetailImage layoutId={layoutId} imageSrc={imageSrc} /> : null}
     </div>
   );
 };
