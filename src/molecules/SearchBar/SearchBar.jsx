917a031ab7c82598a0609c9b81e9db48ec304e6b
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-03-05 09:25:53 +0900

    ✨ [feat] 검색 창 UI 구현

---
+++ src/molecules/SearchBar/SearchBar.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+
+const SearchBar = () => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  const [showSearchInput, setShowSearchInput] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setViewportWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleSearchInput = () => {
+    setShowSearchInput(!showSearchInput)
+  }
+
+  return (
+    <div className="px-[16px]">
+      {/* 기본값 => 352 & 뷰포트 660 => 220 & 뷰포트 550 => 아이콘만 */}
+      {viewportWidth >= 500 ? (
+        <div className={`flex flex-row relative w-[${viewportWidth >= 660 ? 200 : 180}px] items-center`}>
+          <input
+            type="text"
+            className="w-full h-[36px] border-b-2 pl-[5px] outline-none text-sm"
+            placeholder="검색"
+          />
+          {viewportWidth >= 500 && (
+            <button className="absolute right-0 pr-[5px]">
+              <img className='w-[15px] h-[15px]' src="/images/search.svg" alt="Search icon" />
+            </button>
+          )}
+        </div>
+      ) : (
+        <button className="mt-[4px]">
+          <img className='min-w-[20px] min-h-[20px]' src="/images/search.svg" alt="Search icon" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default SearchBar;
