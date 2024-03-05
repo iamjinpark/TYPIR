bb26e4d152c7b41ef686d4ddee5e8b7e87bc36b3
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-03-05 17:21:49 +0900

    🎨 [modify] 프로필 관련 모달 수정

--- src/molecules/UserModal/UserModal.jsx
+++ src/molecules/UserModal/UserModal.jsx
@@ -23,20 +23,45 @@ const UserModal = ({ onClose }) => {
   }
 
   return (
+    // <>
+    //   <div className="fixed bottom-0 w-[290px] h-[200px] flex justify-center items-center z-50">
+    //     <div className="relative bg-white w-[290px] h-[200px] border border-black rounded-xl flex justify-center items-center">
+    //       <CloseButton onClose={onClose} />
+    //       <ul className="flex gap-20">
+    //         <ModalButton onClick={handleCopyLink} href="#" fileName="linkCopy" buttonText="링크 복사"/>
+    //         <ModalButton onClick={handleReport} href="#" fileName="report" buttonText="신고"/>
+    //       </ul>
+    //     </div>
+    //   </div>
+    //   <div className="absolute">
+    //     {showCopyMessage && ( <img src="/images/copyMessage.svg" /> )}       
+    //     {showReportMessage && ( <img src="/images/reportMessage.svg"/> )}
+    //   </div>
+    // </>
+
     <>
-      <div className="fixed bottom-0 w-[290px] h-[200px] flex justify-center items-center z-50">
+      <div className="fixed inset-0 flex justify-center items-center">
+        <div className="w-[290px] h-[200px] flex justify-center items-center z-50">
         <div className="relative bg-white w-[290px] h-[200px] border border-black rounded-xl flex justify-center items-center">
-          <CloseButton onClose={onClose} />
-          <ul className="flex gap-20">
-            <ModalButton onClick={handleCopyLink} href="#" fileName="linkCopy" buttonText="링크 복사"/>
-            <ModalButton onClick={handleReport} href="#" fileName="report" buttonText="신고"/>
-          </ul>
+            <CloseButton onClose={onClose}/>
+            <ul className="flex gap-12">
+              <ModalButton href="#" fileName="edit" buttonText="수정"/>
+              <ModalButton href="#" fileName="delete" buttonText="삭제"/>
+              <ModalButton onClick={handleCopyLink} href="#" fileName="linkCopy" buttonText="링크 복사"/>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="absolute">
-        {showCopyMessage && ( <img src="/images/copyMessage.svg" /> )}       
-        {showReportMessage && ( <img src="/images/reportMessage.svg"/> )}
-      </div>
+      {showCopyMessage && (
+        <div className="absolute flex justify-center items-center z-50">
+          <img src="/images/copyMessage.svg"/>
+        </div>
+      )}
+      {showReportMessage && (
+        <div className="absolute flex justify-center items-center z-50">
+          <img src="/images/reportMessage.svg"/>
+        </div>
+      )}      
     </>
   );
 };
