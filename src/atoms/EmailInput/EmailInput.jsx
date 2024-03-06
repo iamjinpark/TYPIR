f68bcc9904096d065a034f32321c42d82670458d
yeseul0000 <fiddich999@gmail.com>
2024-03-06 09:47:40 +0900

    🐛 [fix] EmailInput 기능 수정 FRONTENDSCHOOL8#4

--- src/atoms/EmailInput/EmailInput.jsx
+++ src/atoms/EmailInput/EmailInput.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const EmailInput = ({ className, onValidationChange }) => {
+const EmailInput = ({ className, onValidationChange, onChange }) => {
   const [email, setEmail] = useState('');
   const [isTouched, setIsTouched] = useState(false);
   const [isValid, setIsValid] = useState(true);
@@ -31,7 +31,10 @@ const EmailInput = ({ className, onValidationChange }) => {
         type="text"
         id="emailInput"
         value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        onChange={(e) => {
+          setEmail(e.target.value);
+          onChange(e.target.value);
+        }}
         className={`w-72 h-11 mt-1 border ${isValid ? 'border-black' : 'border-red-500'} block rounded-2xl px-4`}
         placeholder="email@example.com"
       />
