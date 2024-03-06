60d5315923633d0a2dd47751309adc8d7857e040
Lulurem <smile71109@naver.com>
2024-03-06 16:05:07 +0900

    ✨ [feat] 마이페이지 앨범 상태 스토어로 연결 #63

--- src/zustand/useStore.js
+++ src/zustand/useStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 /* 마이 페이지, 헤더 관련 */
 export const useSelectCategoryStore = create((set) => ({
@@ -25,20 +26,31 @@ export const useToggleStore = create((set) => ({
 }));
 
 /* 포켓베이스 데이터 관련 */
-// export const useImageStore = create((set) => ({
-//   images: [],
-//   setImages: (data) => set({ images: data }),
-// }));
 
+/* 이미지 url 저장 */
 export const useImageStore = create((set) => ({
   selectedImageUrl: null,
   setSelectedImageUrl: (url) => set({ selectedImageUrl: url }),
 }));
 
+/* 마이 페이지 앨범 */
 export const useAlbumStore = create((set) => ({
   albums: [],
   setAlbums: (albums) => set({ albums }),
 }));
+
+/* New Post 앨범 */
+export const usePersistentAlbumStore = create(
+  persist(
+    (set) => ({
+      albums: [],
+      setAlbums: (albums) => set({ albums }),
+    }),
+    {
+      name: 'album-storage', // 로컬 스토리지에 저장될 때 사용할 키
+    },
+  ),
+);
 
 export const useBoardStore = create((set) => ({
   boards: [],
