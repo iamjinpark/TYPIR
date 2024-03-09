6dff75ba53559d613b774e3f3cc76361ab6339da
Lulurem <smile71109@naver.com>
2024-03-10 04:15:19 +0900

    🎨 [modify] 닉네임, 핸들 인풋 validation 리팩토링 #26

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -4,14 +4,48 @@ import CommonInput from '@/atoms/CommonInput/CommonInput';
 import HandleText from '@/atoms/HandleText/HandleText';
 import ProfileImage from '@/atoms/ProfileImage/ProfileImage';
 import TextContents from '@/atoms/TextContents/TextContents';
-import { useState } from 'react';
+import { getPbImage } from '@/utils';
+import { useProfileStore } from '@/zustand/useStore';
+import { useEffect } from 'react';
+
+const isSpecialCharPresent = (string) => {
+  return /[^a-zA-Z0-9_ㄱ-힣]/.test(string); // 영문자, 숫자, 언더스코어 이외의 문자가 있는지 체크
+};
 
 function EditProfile() {
-  const [nickname, setNickname] = useState('');
-  const [handle, setHandle] = useState('');
+  const {
+    profiles,
+    setProfiles,
+    userName,
+    setUserName,
+    handle,
+    setHandle,
+    nameValid,
+    handleValid,
+    imageUrl,
+    setImageUrl,
+  } = useProfileStore();
 
-  const isNameValid = /^[a-zA-Z]+$/.test(nickname) && nickname.length >= 3 && nickname.length <= 16;
-  const isHandleValid = /^[a-zA-Z]+$/.test(handle) && handle.length >= 3 && handle.length <= 16;
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      const user = JSON.parse(storedUser);
+      setProfiles([user]);
+      setUserName(user.userName);
+      setHandle(user.handle);
+      setImageUrl(
+        getPbImage({
+          collectionId: '_pb_users_auth_',
+          // id: user.id,
+          id: '7celmvekv3wfnm7',
+          image: user.profile,
+        }),
+      );
+    }
+  }, [setProfiles, setUserName, setHandle, setImageUrl]);
+
+  const isNameValid = nameValid(userName);
+  const isHandleValid = handleValid(handle);
 
   return (
     <div className="w-full h-[570px] bg-white flex flex-col items-center mb-8">
@@ -22,19 +56,31 @@ function EditProfile() {
         <TextContents text="프로필 수정" fontWeight="font-bold" fontSize="text-[20px]" />
         <div className="flex-1"></div>
       </div>
-      <ProfileImage />
-      <CommonInput
-        text="닉네임"
-        value={nickname}
-        onChange={(e) => setNickname(e.target.value)}
-        placeholder="커뮤니티에서 사용할 닉네임"
-      />
-      <div className="text-red-500 text-xs h-1 -translate-x-7">
-        {!isNameValid && nickname.length > 0 && '3글자 이상, 16글자 이하의 영문이어야 합니다.'}
+      <ProfileImage imageUrl={imageUrl} />
+
+      <div className="nickname-input-wrapper">
+        <CommonInput
+          text="닉네임"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          placeholder="커뮤니티에서 사용할 닉네임"
+        />
+        <div className="text-red-500 text-xs h-1 mt-1 ml-1">
+          {!isNameValid && '3글자 이상, 16글자 이하의 영문, 숫자만 사용 가능합니다.'}
+        </div>
       </div>
-      <CommonInput text="핸들" value={handle} onChange={(e) => setHandle(e.target.value)} placeholder="개인페이지 ID" />
-      <div className="text-red-500 text-xs h-1 -translate-x-7 mb-9">
-        {!isHandleValid && handle.length > 0 && '3글자 이상, 16글자 이하의 영문이어야 합니다.'}
+
+      <div className="handle-input-wrapper">
+        <CommonInput
+          text="핸들"
+          value={handle}
+          onChange={(e) => setHandle(e.target.value)}
+          placeholder="개인페이지 ID"
+        />
+        <div className="text-red-500 text-xs h-1 mt-1 mb-9 ml-1">
+          {!isHandleValid && !isSpecialCharPresent(handle) && '3글자 이상, 16글자 이하의 영문, 숫자만 사용 가능합니다.'}
+          {isSpecialCharPresent(handle) && '특수문자는 언더스코어(_)만 허용됩니다.'}
+        </div>
       </div>
 
       <CommonButton />
