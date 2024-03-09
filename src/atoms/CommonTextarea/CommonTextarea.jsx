3628f974f4e2274237f90eb3014cd4aef347b33f
iamjinpark <wldls7754@gmail.com>
2024-03-10 03:32:56 +0900

    ✨ [feat] : 보드 페이지 레이아웃 구성 완료

--- src/atoms/CommonTextarea/CommonTextarea.jsx
+++ src/atoms/CommonTextarea/CommonTextarea.jsx
@@ -4,12 +4,14 @@ const CommonTextarea = ({
   borderColor = 'border-gray-200',
   margin = 'mt-[10px]',
   padding = 'p-[8px]',
+  height = 'h-[140px]',
+  className,
 }) => {
   return (
     <textarea
       placeholder="내용을 입력해주세요."
       rows={5}
-      className={`${width} border ${borderColor} ${border} ${padding} ${margin}`}
+      className={`${width} border ${borderColor} ${border} ${padding} ${margin} ${height} ${className}`}
     />
   );
 };
---
+++ src/molecules/CategoryButton/CategoryButton.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const CATEGORIES = ['all', 'simple', 'daily', 'vintage'];
+
+const CategoryButtons = ({ margin = 'mt-15px' }) => {
+  const [selectedCategory, setSelectedCategory] = useState('all');
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <div className="flex">
+      {CATEGORIES.map((category) => (
+        <button
+          key={category}
+          className={`px-[0.5625rem] border border-gray-200 rounded-xl cursor-pointer ${margin}
+            ${selectedCategory === category ? 'bg-black text-white' : 'bg-white text-gray-200'} 
+            ${category !== 'all' && 'ml-3'}`} // 'all' 버튼 이외에는 오른쪽 여백 추가
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryButtons;
