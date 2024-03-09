840c34f1b5fe1b4d1dc6a1ee650a1a6faea8a441
dannxnni <132893321+dannxnni@users.noreply.github.com>
2024-03-09 22:53:41 +0900

    🎨 [modify] 커뮤니티 페이지 수정 중

--- src/molecules/CommunityCategory/CommunityCategory.jsx
+++ src/molecules/CommunityCategory/CommunityCategory.jsx
@@ -1,12 +1,13 @@
 import pb from "@/api/pocketbase";
 import { useEffect, useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink, useLocation, useMatch, useNavigate } from "react-router-dom";
 import CommunityImageTemplate from "../CommunityImageTemplate/CommunityImageTemplate";
+import CommunityDetail from "@/pages/CommunityDetail/CommunityDetail";
+import { useStyleStore } from '@/zustand/useStyleStore';
 
 const CATEGORIES = ["all", "simple", "daily", "vintage"]
 
-
-function getPbImageURL (item, fileName = "images") {
+function getPbImageURL (item, fileName = "image") {
   return `${import.meta.env.VITE_PB_API}/api/files/${item.collectionId}/${item.id}/${item[fileName]}`
 }
 
@@ -16,15 +17,27 @@ const CommunityCategory = ({ gap = "gap-3" }) => {
   const location = useLocation()  // 현재 정보 가져오기
   const searchParams = new URLSearchParams(location.search) // URLSearchParams 객체 : URL에서 쿼리 파라미터 추출
 
+
+  const categoryImageMatch = useMatch('/community/detail/:imageId');
+  const layoutId = categoryImageMatch?.params.imageId;
+  const styles = useStyleStore((state) => state.styles);
+  const imageSrc = styles.find((style) => style.id === layoutId)?.image;
+  
+  console.log("categoryImageMatch : ", categoryImageMatch)
+  console.log("layoutId : ", layoutId)
+  console.log("styles : ", styles)
+  console.log("imageSrc : ", imageSrc)
+
   useEffect(() => {
     async function fetchImage() {
       try {
         const styles = await pb.collection("communityPage").getFullList()
         const stylesWithImages = styles.map(style => {
           const imageURL = getPbImageURL(style)
-
+          
           return {...style, image:imageURL}
         })
+        useStyleStore.getState().setStyles(stylesWithImages)
         setImages(stylesWithImages)
       } catch (err) {
         console.error("Error fetching images : ", err)
@@ -47,6 +60,7 @@ const CommunityCategory = ({ gap = "gap-3" }) => {
     setSelectedCategory(category)
   }
 
+
   return (
     <div className="template">
       <div className="mt-[5px] mb-[15px] w-full ">
@@ -59,7 +73,9 @@ const CommunityCategory = ({ gap = "gap-3" }) => {
           </li>
         ))}
       </ul>
-      <CommunityImageTemplate data={filteredImages}/>
+      <CommunityImageTemplate data={filteredImages} />
+      {/* <CommunityDetail imageSrc={imageSrc}/> */}
+      {categoryImageMatch && <CommunityDetail imageSrc={imageSrc}/>}
       </div>
     </div>
   );
