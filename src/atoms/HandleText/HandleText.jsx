3f03c5f7afb5cc770bd5758693138c2784eb79c0
Lulurem <smile71109@naver.com>
2024-03-10 04:41:13 +0900

    🎨 [modify] 프로필 데이터 출력 관련 코드 수정 #11

--- src/atoms/HandleText/HandleText.jsx
+++ src/atoms/HandleText/HandleText.jsx
@@ -1,4 +1,4 @@
-function HandleText({ fontFamily = 'font-sans', fontSize = 'text-[11px]', text = 'girls_day' }) {
+function HandleText({ fontFamily = 'font-sans', fontSize = 'text-[12px]', text = 'girls_day' }) {
   return <div className={`${fontFamily} text-gray-200 ${fontSize}`}>{text}</div>;
 }
 export default HandleText;
--- src/atoms/ProfileUserName/ProfileUserName.jsx
+++ src/atoms/ProfileUserName/ProfileUserName.jsx
@@ -1,11 +1,11 @@
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function ProfileUserName({ text = 'username' }) {
+function ProfileUserName({ text = 'username', isPrivate = false }) {
   return (
     <div>
       <div className="font-serif font-bold text-[23px] inline mr-2">{text}</div>
-      <FontAwesomeIcon icon={faLock} size="xs" />
+      {isPrivate && <FontAwesomeIcon icon={faLock} size="xs" />}
     </div>
   );
 }
