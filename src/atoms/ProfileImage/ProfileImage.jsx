c3125b6152898472b037c78454752cd781c2c446
Lulurem <smile71109@naver.com>
2024-03-10 05:22:53 +0900

    🎨 [modify] 프로필 선택 시 마이 페이지까지 반영 #3

--- src/atoms/ProfileImage/ProfileImage.jsx
+++ src/atoms/ProfileImage/ProfileImage.jsx
@@ -1,21 +1,18 @@
 import { useRef } from 'react';
-import { create } from 'zustand';
-
-const useStore = create((set) => ({
-  selectedFile: null,
-  setSelectedFile: (file) => set({ selectedFile: file }),
-}));
+import { useFileStore } from '@/zustand/useStore';
 
 function ProfileImage({
   width = 'w-[120px]',
   height = 'h-[120px]',
   border = 'rounded-full',
   bgColor = 'bg-gray-100',
+  margin,
   editable = true,
+  imageUrl,
 }) {
   const fileInputRef = useRef();
-  const selectedFile = useStore((state) => state.selectedFile);
-  const setSelectedFile = useStore((state) => state.setSelectedFile);
+  const selectedFile = useFileStore((state) => state.selectedFile);
+  const setSelectedFile = useFileStore((state) => state.setSelectedFile);
 
   const handleImageClick = () => {
     if (editable) {
@@ -25,13 +22,17 @@ function ProfileImage({
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
-    setSelectedFile(URL.createObjectURL(file));
+    if (file) {
+      setSelectedFile(URL.createObjectURL(file));
+    }
   };
 
+  const imageSrc = selectedFile || imageUrl;
+
   return (
-    <div className={`${width} ${height} ${border} ${selectedFile ? '' : bgColor}`} onClick={handleImageClick}>
-      {selectedFile && <img src={selectedFile} alt="프로필 이미지" />}
-      <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />
+    <div className={`${width} ${height} ${border} ${margin} ${imageSrc ? '' : bgColor}`} onClick={handleImageClick}>
+      {imageSrc && <img src={imageSrc} alt="프로필 이미지" className={`${width} ${height} ${border}`} />}
+      {editable && <input type="file" ref={fileInputRef} onChange={handleFileChange} style={{ display: 'none' }} />}
     </div>
   );
 }
