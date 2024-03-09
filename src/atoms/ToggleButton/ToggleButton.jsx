6358897d29ddd320bb9123cf77e5fe00fd9d1a21
Lulurem <smile71109@naver.com>
2024-03-10 04:25:57 +0900

    ✨ [feat] 계정관리 페이지 로컬스토리지 계정 정보 출력 #32

--- src/atoms/ToggleButton/ToggleButton.jsx
+++ src/atoms/ToggleButton/ToggleButton.jsx
@@ -1,10 +1,7 @@
-import { useToggleStore } from '@/zustand/useStore';
 import { faToggleOn, faToggleOff } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function ToggleButton() {
-  const { isToggled, toggle } = useToggleStore();
-
+function ToggleButton({ isToggled, toggle }) {
   return (
     <button onClick={toggle}>
       <FontAwesomeIcon icon={isToggled ? faToggleOn : faToggleOff} size="lg" className="mt-3" />
