035f00426febdcd614883bc868db2555b15ca9f6
yeseul0000 <fiddich999@gmail.com>
2024-03-11 08:03:08 +0900

    ✨ [feat] useUserStore 새로운 코드 추가

--- src/zustand/useUserStore.js
+++ src/zustand/useUserStore.js
@@ -1,11 +1,8 @@
 import { create } from 'zustand';
-
 export const useUserStore = create((set) => ({
   user: null,
   setUser: (userInfo) => set({ user: userInfo }),
-  updateUser: (newUserInfo) =>
-    set((state) => ({
-      user: { ...state.user, ...newUserInfo },
-    })),
+  updateUser: (newUserInfo) => set((state) => ({ user: { ...state.user, ...newUserInfo } })),
+  setImage: (imageFile) => set((state) => ({ user: { ...state.user, image: imageFile } })),
   logoutUser: () => set({ user: null }),
 }));
