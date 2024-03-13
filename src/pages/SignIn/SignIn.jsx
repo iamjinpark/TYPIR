b1f1a470e60fca49fc78fedf5e29ae5811d2f9d5
yeseul0000 <fiddich999@gmail.com>
2024-03-13 10:19:23 +0900

    ⚡️ [refactor] 미완성 컴포넌트 삭제 등  FRONTENDSCHOOL8#55

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import AccountPageTitle from '@/atoms/AccountPageTitle/AccountPageTitle';
-import SocialLogin from '@/atoms/SocialLogin/SocialLogin';
 import LoginForm from '@/molecules/LoginForm/LoginForm';
 import AccountActionLink from '@/atoms/AccountActionLink/AccountActionLink';
 
@@ -8,10 +7,9 @@ const SignIn = () => {
   return (
     <div className="flex items-center justify-center min-h-650px">
       <div className="flex flex-col items-center bg-white py-4">
-        <AccountPageTitle className="mt-[75px] mb-[56px]" />
+        <AccountPageTitle className="mt-[40px] mb-[56px]" />
         <LoginForm />
         <AccountActionLink />
-        <SocialLogin className="mt-[58px]" />
       </div>
     </div>
   );
