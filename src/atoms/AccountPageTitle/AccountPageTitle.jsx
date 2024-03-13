c8e51e34ff3311cc50a5d454c2ca8b504fb6e400
yeseul0000 <fiddich999@gmail.com>
2024-03-13 10:04:05 +0900

    💄 [style] 글자크기 조정  FRONTENDSCHOOL8#37

--- src/atoms/AccountPageTitle/AccountPageTitle.jsx
+++ src/atoms/AccountPageTitle/AccountPageTitle.jsx
@@ -1,5 +1,5 @@
 function AccountPageTitle({ text = '로그인', className }) {
-  return <h1 className={`text-4xl ${className}`}>{text}</h1>;
+  return <h1 className={`text-[25px] bold ${className}`}>{text}</h1>;
 }
 
 export default AccountPageTitle;
