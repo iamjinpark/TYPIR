ed451fcffc07b94db12305ffbac4f6a3996cb60a
yeseul0000 <fiddich999@gmail.com>
2024-03-13 10:18:35 +0900

    💄 [style] text 크기 조정 등 FRONTENDSCHOOL8#20

--- src/atoms/SubmitButton/SubmitButton.jsx
+++ src/atoms/SubmitButton/SubmitButton.jsx
@@ -1,9 +1,10 @@
-const SubmitButton = ({ isFormValid, text = '로그인', width = 'w-72', className }) => {
+const SubmitButton = ({ isFormValid, text = '로그인', width = 'w-72', className, style }) => {
   return (
     <button
       type="submit"
       disabled={!isFormValid}
-      className={`${className} ${width} h-11 block rounded-2xl px-4 text-white font-bold ${isFormValid ? 'bg-black' : 'bg-gray-400'}`}
+      className={`${className} ${width} h-11 block rounded-2xl px-4 text-[15px] text-white font-bold ${isFormValid ? 'bg-black' : 'bg-gray-400'}`}
+      style={style}
     >
       {text}
     </button>
