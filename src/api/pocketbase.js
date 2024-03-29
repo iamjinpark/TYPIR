d2f3b30c6722484f4a4a58f74f963b1f0c0731b4
iamjinpark <wldls7754@gmail.com>
2024-03-30 01:14:43 +0900

    🎨 [modify] .env 환경변수 변경

--- src/api/pocketbase.js
+++ src/api/pocketbase.js
@@ -2,6 +2,6 @@ import PocketBase from 'pocketbase';
 
 // 포켓호스트 인스턴스(싱글톤) 반환
 // const pb = new PocketBase(import.meta.env.VITE_PB_API);
-const pb = new PocketBase('https://pocket10.kro.kr');
+const pb = new PocketBase(import.meta.env.VITE_PB_API);
 
 export default pb;
